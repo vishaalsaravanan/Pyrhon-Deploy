1883f8e6251c0c5c550d7f6b2136321c11778249
Vishaal Saravanan <vishaalsaravanan2000@gmail.com>
2021-06-10 10:39:40 +0530

    Added CRON v1.0

--- application.py
+++ application.py
@@ -9,13 +9,12 @@ from sklearn.ensemble import GradientBoostingRegressor,RandomForestRegressor
 from sklearn.model_selection import train_test_split
 import requests
 from pandas_datareader import data
+from apscheduler.schedulers.background import BackgroundScheduler
 from flask import Flask, render_template
 import time
 from datetime import datetime
 
-
 application=Flask(__name__)
-
 
 
 # pip list --format=freeze >requirements.txt
@@ -56,22 +55,92 @@ params = (
     ('download', 'true'),
 )
 
+def daily_routine():
+        models=[]
+        evaluation_metric=np.array([])
+        actual_date = dt.date.today()
+        historic_date=pd.DataFrame()
+        eval_metric=[]
+        stock_size_counter=[]
+        past_date = actual_date - dt.timedelta(days=365 * 3)
+        actual_date = actual_date.strftime("%Y-%m-%d")
+        past_date = past_date.strftime("%Y-%m-%d")
+        filtered_tickers = get_tickers_filtered(mktcap_min=100000, mktcap_max=10000000)
+        filtered_tickers=list(dict.fromkeys(filtered_tickers))
+        total_tickers=len(filtered_tickers)
+        model_error=[]
+        add_metric=np.array([])
+        final_stock_tickers=[]
+        models=["Stepwise","Svm","Lasso","Ridge","Boosted","forest"]
+        # models.append(out)
+        for model in models:
+            count=0
+            model_name=model
+            predicted_df=np.array([])
+            historic_data=np.array([])
+            for stock_symbol in filtered_tickers:
+                count+=1
+                
+                if count<=total_tickers:
+                    prev_data=pd.DataFrame()
+                    print(' {} {}/{} '.format(stock_symbol,count,total_tickers))    
+                    dataframe = get_stock_data(stock_symbol, past_date, actual_date)
+                    prev_data=dataframe.copy()
+                    prev_dates=dataframe.copy()
+                    # moving data index
+                    prev_dates=prev_dates.reset_index()
+                    prev_dates=prev_dates['Date']
+                    historic_date=prev_dates
+                    
+                    prev_data=prev_data['Close'].values.tolist()
+                    
+                    prev_data=np.array([prev_data]).T
+                    stock_size_counter.append(prev_data.size)
+                    if (len(stock_size_counter)==1 or stock_size_counter[-2]==stock_size_counter[-1]):
+                        
+                        historic_data=np.hstack([historic_data,prev_data]) if historic_data.size else prev_data
+                        (dataframe, forecast_out,pred_arr,date_field,metrics_df) = stock_forecasting(dataframe,model,stock_symbol)
+                        
+                        df_out_metric=metric_calculation(metrics_df,count,total_tickers,model)
+                        if(count==total_tickers):
+                            add_metric=np.hstack([add_metric,np.array(df_out_metric.values.tolist())]) if add_metric.size else np.array(df_out_metric.values.tolist())   
+                        pred_values=np.array([pred_arr]).T
+                     
+                        final_stock_tickers.append(stock_symbol)
+                        metric_values=np.array([metrics_df]).T
+                        evaluation_metric=np.hstack([evaluation_metric,metric_values]) if evaluation_metric.size else metric_values
+                        predicted_df=np.hstack([predicted_df,pred_values]) if predicted_df.size else pred_values
+                   
+            final_dataframe=pd.DataFrame(predicted_df)
+            historic_dataframe=pd.DataFrame(historic_data)
+            final_stock_tickers=list(dict.fromkeys(final_stock_tickers))
+            historic_dataframe.columns=final_stock_tickers
+            historic_dataframe.insert(0,"Date",historic_date.values.tolist())
+            historic_dataframe['Date']=pd.to_datetime(historic_dataframe['Date'])
+
+            historic_dataframe=historic_dataframe.drop(len(historic_dataframe)-1)
+            
+            final_dataframe.columns=final_stock_tickers
+            final_dataframe.insert(0,"Date",date_field)
+            historic_dataframe=pd.concat([historic_dataframe,final_dataframe])
+            historic_dataframe.to_csv("static/"+model_name+"Dataset.csv") 
+           
+        add_df=pd.DataFrame(add_metric)
+        add_df.to_csv("static/Final_Metric.csv")
+        return "Scheduler Call Successfull"
+
+
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(daily_routine,'interval',minutes=1)
+sched.start()
+
 @application.route('/')
 def index():
     
-    a=5
-    b=100
-    #return "Hello,Beautiful"
-    sum=add(a,b)
     df=stock_model()
-    #dataf=pd.read_csv("static/Dataset.csv")
-    #dataf=dataf.to_json()
-    #str1=json.loads(dataf)
     return render_template("index.html",data=df)
-    # return str(a+b)
-    # return sum
-    # return df.to_html(header="true", table_id="table")
-   
+
 
 def add(a,b):
     return str(a+b)
@@ -320,79 +389,6 @@ def choose_rf(dataframe,rf):
     # dataset_to_open=pd.read_csv("static/"+final_model+"Dataset.csv")
     return final_model
 
-def daily_routine():
-        models=[]
-        evaluation_metric=np.array([])
-        actual_date = dt.date.today()
-        historic_date=pd.DataFrame()
-        eval_metric=[]
-        stock_size_counter=[]
-        past_date = actual_date - dt.timedelta(days=365 * 4)
-        actual_date = actual_date.strftime("%Y-%m-%d")
-        past_date = past_date.strftime("%Y-%m-%d")
-        filtered_tickers = get_tickers_filtered(mktcap_min=100000, mktcap_max=10000000)
-        filtered_tickers=list(dict.fromkeys(filtered_tickers))
-        total_tickers=len(filtered_tickers)
-        model_error=[]
-        add_metric=np.array([])
-        final_stock_tickers=[]
-        models=["Stepwise","Svm","Lasso","Ridge","Boosted","forest"]
-        # models.append(out)
-        for model in models:
-            count=0
-            model_name=model
-            predicted_df=np.array([])
-            historic_data=np.array([])
-            for stock_symbol in filtered_tickers:
-                count+=1
-                
-                if count<=total_tickers:
-                    prev_data=pd.DataFrame()
-                    print(' {} {}/{} '.format(stock_symbol,count,total_tickers))    
-                    dataframe = get_stock_data(stock_symbol, past_date, actual_date)
-                    prev_data=dataframe.copy()
-                    prev_dates=dataframe.copy()
-                    # moving data index
-                    prev_dates=prev_dates.reset_index()
-                    prev_dates=prev_dates['Date']
-                    historic_date=prev_dates
-                    
-                    prev_data=prev_data['Close'].values.tolist()
-                    
-                    prev_data=np.array([prev_data]).T
-                    stock_size_counter.append(prev_data.size)
-                    if (len(stock_size_counter)==1 or stock_size_counter[-2]==stock_size_counter[-1]):
-                        
-                        historic_data=np.hstack([historic_data,prev_data]) if historic_data.size else prev_data
-                        (dataframe, forecast_out,pred_arr,date_field,metrics_df) = stock_forecasting(dataframe,model,stock_symbol)
-                        
-                        df_out_metric=metric_calculation(metrics_df,count,total_tickers,model)
-                        if(count==total_tickers):
-                            add_metric=np.hstack([add_metric,np.array(df_out_metric.values.tolist())]) if add_metric.size else np.array(df_out_metric.values.tolist())   
-                        pred_values=np.array([pred_arr]).T
-                     
-                        final_stock_tickers.append(stock_symbol)
-                        metric_values=np.array([metrics_df]).T
-                        evaluation_metric=np.hstack([evaluation_metric,metric_values]) if evaluation_metric.size else metric_values
-                        predicted_df=np.hstack([predicted_df,pred_values]) if predicted_df.size else pred_values
-                   
-            final_dataframe=pd.DataFrame(predicted_df)
-            historic_dataframe=pd.DataFrame(historic_data)
-            final_stock_tickers=list(dict.fromkeys(final_stock_tickers))
-            historic_dataframe.columns=final_stock_tickers
-            historic_dataframe.insert(0,"Date",historic_date.values.tolist())
-            historic_dataframe['Date']=pd.to_datetime(historic_dataframe['Date'])
-
-            historic_dataframe=historic_dataframe.drop(len(historic_dataframe)-1)
-            
-            final_dataframe.columns=final_stock_tickers
-            final_dataframe.insert(0,"Date",date_field)
-            historic_dataframe=pd.concat([historic_dataframe,final_dataframe])
-            historic_dataframe.to_csv("static/"+model_name+"Dataset.csv") 
-           
-        add_df=pd.DataFrame(add_metric)
-        add_df.to_csv("static/Final_Metric.csv")
-        return "hello fellas"
 
 
 def stock_model():
@@ -408,9 +404,7 @@ def stock_model():
         df_org=pd.read_csv("static/"+model_name+"Dataset.csv")
         df_org=df_org.drop(["Unnamed: 0"],axis=1)
 
-        # df_h=pd.read_csv("static/"+"Historic_Data.csv")
-        # df_h=df_h.drop(["Unnamed: 0"],axis=1)
-        
+
         # # df=historic_dataframe.copy()
         # #Pass on to the recommendation function
         recommendation_array=recommendation_model(df_org, rf)
